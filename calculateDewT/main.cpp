348c6bb0edf73589cf66bde48ca2647f33d2e9ff
李永楠|Morgan|Léon <14126123@bjtu.edu.cn>
2015-11-27 01:55:41 +0800

    饱和 单词拼写错误

--- calculateDewT/main.cpp
+++ calculateDewT/main.cpp
@@ -19,12 +19,12 @@ double conversionT_K2C(double temperature_K);
 
 double conversion_P_lgmmHg2kPa(double lgp);
 
-double lgsvp(double satsaturationTemperatureH2O);
+double lgsvp(double saturationTemperatureH2O);
 
 
-double satsaturationTemperatureH2O(double satsaturationPressureH2O_kPa);
+double saturationTemperatureH2O(double saturationPressureH2O_kPa);
 
-double dewTLiBr(double satsaturationTemperatureH2O_C, double concentrationOfLiBrSolution);
+double dewTLiBr(double saturationTemperatureH2O_C, double concentrationOfLiBrSolution);
 
 double enthalpyLiBrSolution(double solutionTemperatureLiBr_C,double concentrationOfLiBrSolution);
 
@@ -59,7 +59,7 @@ int main(int argc, const char * argv[]) {
 ////--求水的饱和蒸汽温度
     cout << "请输入当前压力（kPA）\n";
     cin >> currentPressure;
-    cout << "饱和温度T =" << satsaturationTemperatureH2O(currentPressure) << "˚C\n";
+    cout << "饱和温度T =" << saturationTemperatureH2O(currentPressure) << "˚C\n";
 //
 //--求溴化锂溶液的露点温度
     cout << "\n输入压力为p时水的饱和温度（摄氏度，0℃ < t < 100℃）\n";
@@ -101,6 +101,7 @@ int main(int argc, const char * argv[]) {
 //----------------------------------------------------------------------------------
 
 /*
+ #01
  摄氏度转开尔文温度
  */
 double conversionT_C2K(double temperature_C){
@@ -108,6 +109,7 @@ double conversionT_C2K(double temperature_C){
 }
 
 /*
+ #02
  开尔文温度转摄氏度
  */
 double conversionT_K2C(double temperature_K){
@@ -115,32 +117,36 @@ double conversionT_K2C(double temperature_K){
 }
 
 /*
+ #03
  mmHg与kPa换算
  */
 double conversion_P_lgmmHg2kPa(double lgp){
     return  pow(10, lgp)*0.1333224;
 }
 
-/*计算水的饱和蒸汽压力
+/*
+ #04
+ 计算水的饱和蒸汽压力  公式来源 硕士论文《溴化锂热泵设计》- 杨玉华  （1）
   Calculate saturated vapor pressure(svp) of H2O.
   lgsvp，lg为以10为底的对数
 */
-double lgsvp(double satsaturationTemperatureH2O_K){
+double lgsvp(double saturationTemperatureH2O_K){
     double lgsvp;
-    double t = satsaturationTemperatureH2O_K;
+    double t = saturationTemperatureH2O_K;
     double lgT = log10(t);
     lgsvp = 31.46559 - 8.2*lgT - 3142.305/t + 0.0024804*t;
     return lgsvp;
 }
 
-/*计算水的饱和蒸汽压力
+/*
+ 计算水的饱和蒸汽压力  公式来源 本科生毕业论文《第一类溴化锂吸收式热泵设计.doc》
  Calculate saturated vapor pressure(svp) of H2O (˚C).
  返回kPa
  此公式来误差太大 t=27 时 p=2906.39kpa.
  */
-//double svp(double satsaturationTemperatureH2O_C){
+//double svp(double saturationTemperatureH2O_C){
 //    double p;
-//    double t = satsaturationTemperatureH2O_C;
+//    double t = saturationTemperatureH2O_C;
 //    double a = 9.4865;
 //    double b = 389.27/(42.6676-t-273);
 //    p=exp(a+b);
@@ -152,23 +158,25 @@ double lgsvp(double satsaturationTemperatureH2O_K){
  Calculate saturated vapor temperature(svp) of H2O.
  适用于0.1Mpa- 0.35Mpa 相对误差≤0.3%
  */
-//double satsaturationTemperatureH2O(double satsaturationPressureH2O_kPa){
+//double saturationTemperatureH2O(double saturationPressureH2O_kPa){
 //    double t;
-//    double p = satsaturationPressureH2O_kPa/1000;
+//    double p = saturationPressureH2O_kPa/1000;
 //    
 //    t = 189.2000 * sqrt(sqrt(p)) + -6.5;
 //
 //    return t;
 //}
 
-/*计算水的饱和温度 公式来源《蒸发过程饱和水蒸气对应参数的计算方程》-高俊
+/*
+ #05
+ 计算水的饱和温度 公式来源《蒸发过程饱和水蒸气对应参数的计算方程》-高俊
  Calculate saturated vapor temperature(svp) of H2O.
  适用于2kpa- 1500kpa 相对误差≤0.3%
  按此公式与上述压力计算公式反算 每个温度值与标准值几乎都相差0.4，遂在计算末加之
  */
-double satsaturationTemperatureH2O(double satsaturationPressureH2O_kPa){
+double saturationTemperatureH2O(double saturationPressureH2O_kPa){
     double t = 0.0000000;
-    double p = satsaturationPressureH2O_kPa;
+    double p = saturationPressureH2O_kPa;
     double c[] = {6.004553,15.89247,-0.1723261,0.3940772,-4.631586e-2,3.01707e-3};
     for (int i = 0; i < 6; i++) {
         t += c[i]*pow(log(p), i);
@@ -177,19 +185,39 @@ double satsaturationTemperatureH2O(double satsaturationPressureH2O_kPa){
     return t+0.4;
 }
 
+/*
+ #06
+ 计算饱和水的气化潜热(latent heat of vaporization)
+ r = 597.34 - 0.555t1 - 0.2389 * 10^y
+ y = 5.1463 - 1540/(t1+273.16)
+ 
+ t1 为压力p时饱和水蒸气的温度，可利用#05计算
+ */
+double _latentHeatOfVaporization_H2O(double satsaturationTemperatureH2O ){
+    return 0;
+}
+
+
+/*
+ 计算水或水蒸气的焓值
+ */
+
+
+
+
+
 /*计算溴化锂的露点温度(Calculate dew temperature of LiBr)
 
-    satsaturationTemperatureH2O_C:水的饱和温度（摄氏度）
+    saturationTemperatureH2O_C:水的饱和温度（摄氏度）
     concentrationOfLiBrSolution :100kg 溴化锂水溶液中含有溴化锂的千克数
  此方程试用范围：
-    0℃ < satsaturationTemperatureH2O_C < 100℃
+    0℃ < saturationTemperatureH2O_C < 100℃
     45% < concentrationOfLiBrSolution < 65%
     
     x = 61.7, t = 75 ; dewTLiBr = 17.64793616
  
 */
-
-double dewTLiBr(double satsaturationTemperatureH2O_C, double concentrationOfLiBrSolution){
+double dewTLiBr(double saturationTemperatureH2O_C, double concentrationOfLiBrSolution){
     
     double a[]={0.770033,1.45455e-2,-2.63906e-4,2.27609e-6};
     double b[]={140.877,-8.55749,0.16709,-8.82641e-4};
@@ -197,7 +225,7 @@ double dewTLiBr(double satsaturationTemperatureH2O_C, double concentrationOfLiBr
 //      double a[]={0.77,1.455,-2.6401,2.277};
 //      double b[]={140.876,-855.745,1670.89,882.636};
     
-    double t= satsaturationTemperatureH2O_C;
+    double t= saturationTemperatureH2O_C;
     double x = concentrationOfLiBrSolution;
     
     double sum1 =0.000000 ,sum2=0.000000;
@@ -305,7 +333,7 @@ double enthalpyLiBrSolution(double solutionTemperatureLiBr_C,double concentratio
  */
 double _concentration_LiBrSolution(double solutionTemperatureLiBr_C,double pressure_kPa){
     double t = solutionTemperatureLiBr_C;
-    double t1 = satsaturationTemperatureH2O(pressure_kPa);
+    double t1 = saturationTemperatureH2O(pressure_kPa);
     double a[] = {0.31057,-1.282e-2,-1.7312e-4,5.3303e-7};
     double b[] = {1.232e-2,3.846e-4,-7.1457e-8,-5.73e-9};
     double c[] = {-1.9166e-4,-3.334e-6,5.3123e-8,-3.6012e-10,1.0257e-12};
